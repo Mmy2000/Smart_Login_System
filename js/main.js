3c60969cad6e0b9b38e554b23002313639ffd3f8
Mmy2000 <yahianada005@gmail.com>
2024-03-07 15:09:06 +0200

    starting js for the project

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ var signinEmail = document.getElementById("signinEmail")
 var signinPassword = document.getElementById("signinPassword")
 var arrUser ;
 
+var pathparts = location.pathname.split('/');
+var baseURL = ''
+for (var i = 0; i < pathparts.length - 1; i++) {
+    baseURL += '/' + pathparts[i]
+}
+console.log(baseURL);
+
+// to say welcome in home page
+var username = localStorage.getItem('sessionUsername')
+if (username) {
+    document.getElementById('username').innerHTML = "Welcome " + username
+}
+
+
 if (localStorage.getItem("users")) {
     arrUser = JSON.parse(localStorage.getItem("users"));
 } else {
@@ -48,7 +62,7 @@ function register() {
     if (isEmailExist() == false) {
         document.getElementById('test').innerHTML = '<span class="text-danger m-3">email already exists</span>'
     }else {
-        arrUser.push(signUp)
+        arrUser.push(users)
         localStorage.setItem('users', JSON.stringify(arrUser))
         document.getElementById('test').innerHTML = '<span class="text-success m-3">Success</span>'
         console.log(arrUser);
@@ -61,5 +75,28 @@ function isLoginEmpty() {
         return false
     } else {
         return true
+    }
+}
+function login() {
+    if (isLoginEmpty() == false) {
+        document.getElementById('test2').innerHTML = '<span class="text-danger m-3">All fields is required</span>'
+        return false
+    }
+    var password = signinPassword.value
+    var email = signinEmail.value
+    for (let i = 0; i < arrUser.length; i++) {
+        if (arrUser[i].email.toLowerCase() == email.toLowerCase() && arrUser[i].password.toLowerCase() == password.toLowerCase()) {
+            localStorage.setItem('sessionUsername', arrUser[i].name)
+            if (baseURL == '/') {
+                location.replace('https://' + location.hostname + '/home.html')
+
+            } else {
+                location.replace(baseURL + '/home.html')
+
+            }
+        }else{
+            document.getElementById('test2').innerHTML = '<span class="p-2 text-danger">incorrect email or password</span>'
+        }
+        
     }
 }
